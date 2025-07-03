127929834959a830fb0146fe159c0cb3a3db9e69
Leticia Maia <132717041+maia-leticia@users.noreply.github.com>
2025-07-03 19:41:22 -0300

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+       <main className="bg-[#047790] h-[100vh] flex items-center justify-center">
+         <div className="bg-[#FFFFFF] w-[75vw] h-[800px] rounded-[12px]">
+         </div>
+       </main>
+    </>
+  )
+}
+
+export default App

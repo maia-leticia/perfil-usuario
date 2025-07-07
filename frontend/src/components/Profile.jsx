d204010738ef2cc441dfc50b0227d13b0eec01a7
Leticia Maia <132717041+maia-leticia@users.noreply.github.com>
2025-07-07 17:08:39 -0300

    Verificação de campos
    Verificação de campos antes de enviar os dados

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -18,12 +18,21 @@ const [isEditing, setIsEditing] = useState(false)
 
 const [message, setMessage] = useState("")
 
+function editingMode(){
+    setIsEditing(true)
+    setMessage("")
+}
+
 function handleChange(e){
     const{name, value} = e.target
     setUser(prev => ({
         ...prev,
         [name]: value
     }))
+}
+
+function editPhoto(){
+
 }
 
 function handleCancel(){
@@ -36,8 +45,32 @@ function handleCancel(){
      .catch(err => console.error("Erro ao cancelar", err))
 }
 
+
 function handleSave(e){
     e.preventDefault()
+
+    if (
+        !String(user.user_name).trim() ||
+        !String(user.user_age).trim() ||
+        !String(user.user_street).trim() ||
+        !String(user.user_neighborhood).trim() ||
+        !String(user.user_city).trim() ||
+        !String(user.user_bio).trim()
+    ) {
+        setMessage("Erro: Preencha todos os campos.")
+        return
+  }
+
+    if (!user.user_name.trim() || /\d/.test(user.user_name)) {
+        setMessage("Erro: O nome não pode conter números.");
+        return;
+    }
+
+    if (!/^\d+$/.test(user.user_age)) {
+        setMessage("Erro: A idade deve conter apenas números.");
+        return ;
+}
+
     fetch("http://localhost:3001/usuario", {
         method: "POST",
         headers: {
@@ -52,6 +85,8 @@ function handleSave(e){
         setIsEditing(false)
     }) .catch(err=>{
         console.error("Erro ao salvar os dados!", err)
+        setMessage("Erro ao salvar os dados!")
+        setIsEditing(false)
     })
 }
 
@@ -68,10 +103,14 @@ useEffect(()=>{
         <form className="font-poppins py-[40px] px-[54px]" >
             <div className="flex justify-between">
                 <div className="flex items-center gap-10 mb-[20px] lg:mb-[30px]">
-                    <img src={IconProfile} alt="profile-icon" className="rounded-full  w-[100px] lg:w-[160px] h-[100px] lg:h-[160px] object-cover"/>
+                    <img src={user.user_photo_url || IconProfile} alt="profile-icon" className={` ${isEditing ? "hover:cursor-pointer":""} rounded-full  w-[100px] lg:w-[160px] h-[100px] lg:h-[160px] object-cover `}/>
                     <h1 className="hidden sm:block text-[40px]">Meu Perfil</h1>
                 </div>
-                <img src={EditPencil} alt="" onClick={()=>setIsEditing(true)} className="w-[30px] h-[30px] md:w-[40px] md:h-[40px] cursor-pointer" />
+                {
+                    !isEditing && (
+                         <img src={EditPencil} alt="" onClick={editingMode} className="w-[30px] h-[30px] md:w-[40px] md:h-[40px] cursor-pointer" />
+                    )
+                }        
             </div>
             <div className="flex flex-col gap-[10px] text-[15px] lg:text-[24px]">
                 <div className="flex items-center justify-start pl-[24px]">
@@ -95,24 +134,20 @@ useEffect(()=>{
                     <input type="text" name="user_city" value={user.user_city} onChange={handleChange} disabled={!isEditing} className={`flex flex-1 h-[30px] lg:w-[60vw] lg:h-[35px] rounded-[45px] p-2 focus:outline-none ${isEditing ? "border border-[#1A1A1A]" : "text-[#4B5563]"}`} />          
                 </div>
                 <div className={`flex flex-col h-[150px] mt-[15px] mb-2 px-[24px] py-[10px] rounded-[28px] ${isEditing ? "border-[#1A1A1A] border" : ""}`}>
-                    <label htmlFor="user_bio" className="font-semibold mb-[5px]" >Biografia:</label>         
+                    <label htmlFor="user_bio" className="font-semibold mb-[10px]" >Biografia:</label>         
                     <textarea name="user_bio" value={user.user_bio} onChange={handleChange} disabled={!isEditing}  className={`flex flex-1 h-[100%] resize-none focus:outline-none ${isEditing ? "" : "text-[#4B5563]"}`}></textarea>
                 </div>
-                {
-                    isEditing ? 
-                    
-                        <div className="flex justify-end gap-4">
-                            <button className="w-[18vw] cursor-pointer md:h-[48px] h-[35px]  text-[#FFFFFF] rounded-[40px] bg-[#FF0000]" onClick={handleCancel}>Cancelar</button>
-                            <button className="w-[18vw] cursor-pointer md:h-[48px] h-[35px] text-[#FFFFFF] rounded-[40px] bg-[#1AABF4]" onClick={handleSave}>Salvar</button>
-                        </div>
-                     :
-                    
-                    <span className="text-[14px] text-green-600 flex justify-end">{message}</span>
-                    
-
-
-                }
-
+                <div className="flex justify-between items-center">
+                    <span className={`text-[12px] ${message.includes("Erro") ? "text-red-600" : "text-green-600"}`}>{message}</span>
+                    {
+                        isEditing && (
+                            <div className="flex justify-end gap-4">
+                                <button className="w-[18vw] cursor-pointer md:h-[48px] h-[35px]  text-[#FFFFFF] rounded-[40px] bg-[#FF0000]" onClick={handleCancel}>Cancelar</button>
+                                <button className="w-[18vw] cursor-pointer md:h-[48px] h-[35px] text-[#FFFFFF] rounded-[40px] bg-[#1AABF4]" onClick={handleSave}>Salvar</button>
+                            </div>
+                    )}
+                </div>
+                
             </div>
         </form>
     )
